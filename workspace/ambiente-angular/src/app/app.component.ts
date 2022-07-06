db6a37c3ab98cc4c685ee0be4e4b98149963acf4
Fernando <fernando.luiz92@hotmail.com>
2022-07-05 21:08:04 -0300

    Implementando a lógica de incremento do contador

--- workspace/ambiente-angular/src/app/app.component.ts
+++ workspace/ambiente-angular/src/app/app.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  count = 0;
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    let interval = setInterval(()=> {
+      this.count++;
+      if(this.count === 10){
+        clearInterval(interval)
+      }
+    }, 1000)
+  }
 }
